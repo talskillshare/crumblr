dd45356bddcc8b4eefae9e546a444aead775a8f3
Tal Safran <tsafran@gmail.com>
2013-08-23 12:50:27 -0400

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crumblr::Application.config.secret_token = 'ce305481dd0b50cd929fde0648bf602eada454e137b5b427349e351d9efecf0e8c7a9cbe80a403cb25b0990aa2e723d691ad991cc645ebee505a9cca1e74a534'
